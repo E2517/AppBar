ca6510638008152b806d14c988e5e3f4ac52263a
Carlos Caravaca <ccaravac@everis.com>
2020-04-29 11:07:06 +0100

    Version 1.0 Reuse My Personal App Bar

---
+++ README.md
@@ -0,0 +1,7 @@
+# appbar
+
+Flutter - Extend and Reuse the App Bar, if you call the class withour passing arguments the app bar will show a leading(icon), text and action(icon) by default.
+
+If you do not want by default anything you should add in the class appbar_utils after : null
+
+
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:appbar/Utils/appbar_utils.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: MyPersonalAppBar(),
+        body: Center(
+          child: Container(
+            child: Text('Reuse App Bar'),
+          ),
+        ),
+      ),
+    );
+  }
+}
